f7ae7228597aaa183732ca2f814d460b89e806d4
David Pinto Pajares <david.pinto@nter.es>
2024-11-26 11:45:35 +0100

    Añadido .py sobre valores booleanos

---
+++ ejer_boolean/ejer_boolean.py
@@ -0,0 +1,15 @@
+"""
+Define una variable con valor True. --> 1
+Define una variable con valor False. --> 0 (tambien sirve con True, False)
+Define una variable con valor True mediante un entero
+"""
+prim_var = True
+seg_var = False
+
+mi_variable = 0
+
+if mi_variable: 
+    print("Mi variable es verdadera")
+else:
+    print("Mi variable es falsa")
+
