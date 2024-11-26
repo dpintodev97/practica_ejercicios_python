3a3d8e42cdd9081505e3060f59845485654a33ba
David Pinto Pajares <david.pinto@nter.es>
2024-11-26 11:03:27 +0100

    Añado subcarpetas por bloques de ejercicios y el primer bloque de tema numericos

---
+++ ejer_num_1/ejercicios1.py
@@ -0,0 +1,19 @@
+#EJERCICIOS NUMÉRICOS 1 :
+
+"""
+1.Define y muestra un número de tipo int.
+2.Define y muestra un número de tipo float.
+3.Define un número de tipo complex.
+4.Muestra por pantalla la parte real del número definido en el punto 3.
+5.Muestra por pantalla la parte imaginaria del número definido en el punto 3"""
+
+num_int = 5
+print("Numero entero: ", num_int)
+
+num_float = 5.5
+print("Numero flotante: ", num_float)
+
+num_complex = 5j
+print("Numero complejo: ", num_complex)
+print("Numero complejo real: ", num_complex.real)
+print("Numero complejo imaginario: ", num_complex.imag)
