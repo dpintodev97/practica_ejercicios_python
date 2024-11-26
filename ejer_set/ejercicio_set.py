616c1af371a66869e94585e54a99a423b6920bcd
David Pinto Pajares <david.pinto@nter.es>
2024-11-26 13:01:10 +0100

    Añadido ejercicio de set y .txt de List VS Set VS Array

---
+++ ejer_set/ejercicio_set.py
@@ -0,0 +1,6 @@
+#EJERCICIO DE CONJUNTOS (set):
+"""Conjunto (set) : es una colección de elementos no ordenados y sin duplicados. 
+Los conjuntos NO mantienen el orden de los elementos (a partir de Python 3.7, el orden de inserción es preservado, 
+pero no se puede contar con esto en versiones anteriores)."""
+
+mi_set = {1, 2, 3, 4, 5}
