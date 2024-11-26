405845441bec95ff38c966e2f273f522844dd19d
David Pinto Pajares <david.pinto@nter.es>
2024-11-26 14:30:59 +0100

    Añadido ejercicio de tuplas, ejercicio_tupla.py

---
+++ ejer_tuplas/ejercicio_tupla.py
@@ -0,0 +1,7 @@
+#EJERCICIO DE TUPLAS:
+"""Tupla: es una colección de elementos NO ORDENADOS y SIN DUPLICADOS. 
+    Las tuplas son inmutables (no se pueden modificar).
+    A diferencia de los conjuntos (set), las tuplas sí mantienen el orden en que se agregan los elementos.
+    Permiten duplicados: A diferencia de los conjuntos (set), las tuplas sí permiten duplicados."""
+
+
