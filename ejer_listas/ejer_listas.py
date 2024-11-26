722124cf30321f0f066a344ef22fdff641cce7a1
David Pinto Pajares <david.pinto@nter.es>
2024-11-26 12:36:58 +0100

    Actualizado ejercico de listas

--- ejer_listas/ejer_listas.py
+++ ejer_listas/ejer_listas.py
@@ -1,3 +1,10 @@
+#EJERCICIOS DE LISTAS:
+"""
+lista en Python: es un tipo de estructura de datos que permite almacenar una colección de elementos de cualquier 
+    tipo (no como los arrays). Son ordenadas, mutables (puedes cambiar sus elementos después de haberlas creado) 
+    y permiten elementos duplicados.
+"""
+
 mi_lista = [10, 20, "Hello", 20.5]
 
 #Añade “Word” al final de la lista.
@@ -16,3 +23,21 @@ print(mi_lista)
 mi_lista_2 = [20, 40]
 mi_lista_2.extend(["Bye"])
 print(mi_lista_2)
+
+#Une las dos listas y muestra la lista final.
+mi_lista_3 = mi_lista + mi_lista_2
+print(mi_lista_3) 
+
+"""
+PYTHON vs JAVA
+    En Python:
+
+    .Las listas son estructuras de datos dinámicas y flexibles que pueden contener elementos de tipos diversos.
+    .Los arrays de Python son más eficientes para manejar datos homogéneos (todos del mismo tipo), 
+        pero no son tan comunes para la mayoría de los casos como las listas.
+
+    En Java:
+
+    .List es una interfaz que se implementa en clases como ArrayList y LinkedList.
+    .Las listas en Java son genéricas y más orientadas a tipos específicos, lo que proporciona más control 
+        sobre el tipo de elementos que puedes almacenar."""
