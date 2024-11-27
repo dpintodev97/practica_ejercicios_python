9e1eb23bde02aa7e4166fdbef76848ad0401132f
David Pinto Pajares <david.pinto@nter.es>
2024-11-27 11:46:31 +0100

    Añadido bloque de estructuras de control y lambda

---
+++ ejer_est_control_lambdas/ej_est_control_lambdas.py
@@ -0,0 +1,17 @@
+#EJERCICIOS CON IF:
+a = 10
+b = 100
+
+if a == 10:
+    print("Es igual a 10")
+else:
+    print("Es diferente de 10")
+
+
+#EJERCICIOS CON WHILE:
+
+
+#EJERCICIOS CON FOR:
+
+
+#EJERCICIOS CON LISTAS POR COMPRENSION:
