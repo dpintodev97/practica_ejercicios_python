c6712a7ece86fe1d05afd7ac48c22f7ea59af0bd
David Pinto Pajares <david.pinto@nter.es>
2024-11-27 10:58:01 +0100

    Añado bloque de ejercicios de operadores logicos; los distintos temas del bloque estarán en un .py, ejercicios_operadores_logicos.py

---
+++ ejer_operadores_logicos/ejercicios_operadores_logicos.py
@@ -0,0 +1,9 @@
+#EJERCICIOS CON OPERADORES ARITMETICOS
+a, b, c = 50, 5.0, 100  # Asignación múltiple (si se escribe de seguido (a=50, b=5.0....)lo interpreta como una Tupla)
+d = a + b
+e = a - b
+f = d * e
+g = f / a
+h = f % a
+print("Variable soriginales sobre las que hacer operaciones: ", a, b, c, "; y las operaciones: ", d, e, f, g, h)
+
