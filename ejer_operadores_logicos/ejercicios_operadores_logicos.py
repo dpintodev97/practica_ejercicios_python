ec00e3e0196378caf5c395aa283d1b0ab44e7884
David Pinto Pajares <david.pinto@nter.es>
2024-11-27 11:01:44 +0100

    Actualizo ejercicios de operadores logicos

--- ejer_operadores_logicos/ejercicios_operadores_logicos.py
+++ ejer_operadores_logicos/ejercicios_operadores_logicos.py
@@ -7,3 +7,19 @@ g = f / a
 h = f % a
 print("Variable soriginales sobre las que hacer operaciones: ", a, b, c, "; y las operaciones: ", d, e, f, g, h)
 
+#EJERCICIOS OPERADOR DE COMPARACIÓN
+#Define las variables a y b con los valores 50 y 10
+a = 50
+b = 10  
+#Comprueba si a y b son iguales.
+a == b
+print(a == b)
+#Comprueba si a y b son distintos.
+a != b
+print(a != b)
+#Comprueba si a es mayor que b.
+a > b
+print(a > b)
+#Comprueba si a es menor o igual que b.
+a <= b
+print(a <= b)   
